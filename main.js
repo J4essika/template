528f9686d3427e25f5320669a283bd2546b14bb4
Jéssica da Conceiçao <daconceicaoj792@gmail.com>
2022-08-19 13:37:36 -0300

    teste

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 window.alert('Olá,seja bem vindo ao site')
+var user = new Object();
+user.first = "Jessica";
+user.last = "Lawliet";
+
+console.log(user.first, user.last);
+// Output:
+// Jessica Lawliet
+
 
 
 
